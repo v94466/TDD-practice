3b696a5eead1cc389b7dcdb514363a257af6c5a3
v94466 <veerpalkaur46159@gmail.com>
2020-10-16 20:16:36 -0400

    adds first unit test and setup mocha as our testing script

---
+++ test/unitTest.js
@@ -0,0 +1,9 @@
+const chai = require('chai');
+const assert = chai.assert;
+
+describe('sum', function(){
+    it('returns 4 when adding 2 and 2', function(){
+        assert.equal(sum(2,2),4);
+    });
+});
+    
